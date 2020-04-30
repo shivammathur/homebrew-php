4688bede976eb7bf2970c2dc5f79bbf17810c9c1
Shivam Mathur <shivam_jpr@hotmail.com>
2020-04-30 16:20:04 +0530

    Fix typo

--- Formula/php@7.1.rb
+++ Formula/php@7.1.rb
@@ -282,7 +282,7 @@ class PhpAT71 < Formula
 
   plist_options :manual => "php-fpm"
 
-  def plist;
+  def plist
     <<~EOS
       <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
