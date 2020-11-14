595e99e6899c67c58690a403963366b14bd33d95
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-11-14 01:00:46 +0000

    php@8.1: update 8.1.0 bottle.

--- Formula/php@8.1.rb
+++ Formula/php@8.1.rb
@@ -6,9 +6,8 @@ class PhpAT81 < Formula
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/php"
-    rebuild 43
-    sha256 "ca479f758f0b3b7174edd4c28abdd55ac038f26e8fee1add0c1922a3b6b7d11b" => :catalina
-    sha256 "e8227329c524e5f256e87d392e11785919f091495cf2691b748edf3b59349c17" => :big_sur
+    rebuild 44
+    sha256 "0ef4956987cf838bbcfbebf3a60d1954cfab80e62c2931ccf94fb0b480184542" => :catalina
   end
 
   keg_only :versioned_formula
