240d4d49bc2180c1cede6564e319b23008e6dbf0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-26 00:58:54 +0000

    php@8.1: rebuild
    Update php@8.1
    
    Closes #119.
    
    Signed-off-by: BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>

--- Formula/php@8.1.rb
+++ Formula/php@8.1.rb
@@ -1,17 +1,13 @@
 class PhpAT81 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://github.com/php/php-src/archive/master.tar.gz?v=8.1.0&build_time=1611538446"
+  url "https://github.com/php/php-src/archive/master.tar.gz?v=8.1.0&build_time=1611622733"
   version "8.1.0"
-  sha256 "1da1e72b375fd7e43ca79907bc66be28878d8b285ca6e9b781e388d87a314f76"
+  sha256 "e8a9adf7bf7cd8f54729e03f315fb96dbcc4a898b1cfb8296a0cde600604ba57"
   license "PHP-3.01"
 
   bottle do
     root_url "https://dl.bintray.com/shivammathur/php"
-    rebuild 121
-    sha256 "fb4fe6c4678260d51c33c34caee10b9aaca8211905bbbc6db55c8f1954a4dcb9" => :big_sur
-    sha256 "80b8fd3abe06a33d80f521b55200678862d163225fe40610a3628bb76fe794aa" => :arm64_big_sur
-    sha256 "716c593de62e7e0cecbaf49865daeebb114d64179f862cecd83adfc07be7888e" => :catalina
   end
 
   keg_only :versioned_formula
