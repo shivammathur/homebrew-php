235f83fbb338861762dd3944d4ca337bec8315e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-04 07:59:34 +0000

    Update php@7.2

--- Formula/php@7.2.rb
+++ Formula/php@7.2.rb
@@ -2,15 +2,11 @@ class PhpAT72 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
   url "https://github.com/shivammathur/php-src-backports/archive/7.2.34.tar.gz"
-  sha256 "5823190fb9e45b5676dbbb98aa922ed566fa4940492cae13d4fab018e4e23c8b"
+  sha256 "72c9ae7a6b0b55b2837679d8b649e9bb141d5c16eff80d0dd06eae09a999e9bf"
   license "PHP-3.01"
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
-    rebuild 4
-    sha256 arm64_big_sur: "853774aa22235b1c3215ece13660c5134648f4aff4e545068c45dd824dd877d5"
-    sha256 big_sur:       "856c09e52bd1851055b64326a97e6e3ab68a21cdcb3c83ec2dfeeb26791c81ea"
-    sha256 catalina:      "90af43b7a4fbc81982bc2b1376d3251a5498527047391bc260028d8d2cff4043"
   end
 
   keg_only :versioned_formula
