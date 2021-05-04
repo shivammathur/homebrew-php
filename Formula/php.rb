2955944688a5f38280a4d6c8ad21081e2b4afee2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-04 07:38:57 +0000

    Update php

--- Formula/php.rb
+++ Formula/php.rb
@@ -14,10 +14,6 @@ class Php < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
-    rebuild 1
-    sha256 arm64_big_sur: "2751a1d8516488e886ce21358fdacc1a0469c3ccccb321b812578d2e0c3affda"
-    sha256 big_sur:       "3cf1d51ae6c5328504024ba1ac7a68d178dce21eb7344c9b9098ce7d093701ae"
-    sha256 catalina:      "60583ad4f905f2c5bba8686760f936cee4a6fdad124febb2edab885755aa5f8f"
   end
 
   head do
@@ -60,7 +56,6 @@ class Php < Formula
 
   patch do
     url "https://raw.githubusercontent.com/shivammathur/homebrew-php/fc256dac438bde6fc35e57eeeb8803c71a3f6f1a/Patches/php-8.0.5-80892.patch"
-    sha256 "62b82271ef6f54965af1da563bc287ca52af879299b6e086afa66931aaaae6b3"
   end
 
   def install
