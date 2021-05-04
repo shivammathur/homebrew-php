727a8f1ba02463d9952a821fab0516dceea5ac1c
Shivam Mathur <shivam_jpr@hotmail.com>
2021-05-04 11:34:13 +0530

    Automate rebuild of EOL versions on security updates [skip ci]
    Clean up edit.sh

--- .github/scripts/edit.sh
+++ .github/scripts/edit.sh
@@ -1,38 +1,30 @@
 unbottle() {
-  if [[ "$PHP_VERSION" =~ php$|php@7.[3-4] ]]; then
-    printf "  bottle do\n    root_url \"%s\"\n" "$GITHUB_PACKAGES_URL" > /tmp/bottle
-    sed -Ei '/    rebuild.*/d' ./Formula/"$PHP_VERSION".rb
-    sed -Ei '/    sha256.*/d' ./Formula/"$PHP_VERSION".rb
-    sed -Ei '/  revision.*/d' ./Formula/"$PHP_VERSION".rb
-    sed -i -e "/bottle do/r /tmp/bottle" -e "//d" ./Formula/"$PHP_VERSION".rb
-    sudo rm -f /tmp/bottle
-  else
-    if [[ "$PHP_VERSION" =~ php@8.[1-9] ]]; then
-      url="$(grep -e "^  url.*" ./Formula/"$PHP_VERSION".rb | cut -d\" -f 2)"
-      checksum=$(curl -sSL "$url" | shasum -a 256 | cut -d' ' -f 1)
-      commit="$(curl -sL https://api.github.com/repos/php/php-src/commits/master | sed -n 's|^  "sha":.*"\([a-f0-9]*\)",|\1|p')"
-      sed -i -e "s/^  sha256.*/  sha256 \"$checksum\"/g" ./Formula/"$PHP_VERSION".rb
-      sed -i -e "s|commit.*|commit=$commit\"|g" ./Formula/"$PHP_VERSION".rb
-    fi
-    sed -Ei '/    rebuild.*/d' ./Formula/"$PHP_VERSION".rb
-    sed -Ei '/    sha256.*/d' ./Formula/"$PHP_VERSION".rb
-    sed -Ei '/  revision.*/d' ./Formula/"$PHP_VERSION".rb
-  fi
+  sed -Ei '/    rebuild.*/d' ./Formula/"$PHP_VERSION".rb
+  sed -Ei '/    sha256.*/d' ./Formula/"$PHP_VERSION".rb
+  sed -Ei '/  revision.*/d' ./Formula/"$PHP_VERSION".rb
 }
 
-check_url() {
+check_changes() {
   new_url="$(grep -e "^  url.*" ./Formula/"$PHP_VERSION".rb | cut -d\" -f 2)"
-  old_url="$(grep -e "^  url.*" /tmp/"$PHP_VERSION".rb | cut -d\" -f 2)"
+  old_url="$(grep -e "^  url.*" ./tmp/"$PHP_VERSION".rb | cut -d\" -f 2)"
+  new_checksum="$(grep -e "^  sha256.*" /Formula/"$PHP_VERSION".rb.new | cut -d\" -f 2)"
+  old_checksum="$(grep -e "^  sha256.*" /tmp/"$PHP_VERSION".rb.new | cut -d\" -f 2)"
   echo "new_url: $new_url"
   echo "old_url: $old_url"
-  if [ "$new_url" = "$old_url" ]; then
+  echo "new_checksum: $new_checksum"
+  echo "old_checksum: $old_checksum"
+  if [ "$new_url" = "$old_url" ] && [ "$new_checksum" = "$old_checksum" ]; then
     sudo cp /tmp/"$PHP_VERSION".rb Formula/"$PHP_VERSION".rb
   fi
 }
 
 fetch() {
   sudo cp "Formula/$PHP_VERSION.rb" "/tmp/$PHP_VERSION.rb"
-  if [[ "$PHP_VERSION" =~ php$|php@7.[3-4] ]]; then
+  if [[ "$PHP_VERSION" =~ php@(5.6|7.[0-2]) ]]; then
+    url="$(grep -e "^  url.*" ./Formula/"$PHP_VERSION".rb | cut -d\" -f 2)"
+    checksum=$(curl -sSL "$url" | shasum -a 256 | cut -d' ' -f 1)
+    sed -i -e "s|^  sha256.*|  sha256 \"$checksum\"|g" ./Formula/"$PHP_VERSION".rb
+  elif [[ "$PHP_VERSION" =~ php$|php@7.[3-4] ]]; then
     status_code=$(sudo curl -w "%{http_code}" -o "/tmp/$PHP_VERSION.rb.new" -sL "https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/$PHP_VERSION.rb")
     if [ "$status_code" = "200" ]; then
       url="$(grep -e "^  url.*" /tmp/"$PHP_VERSION".rb.new | cut -d\" -f 2)"
@@ -44,6 +36,12 @@ fetch() {
     else
       sudo cp "/tmp/$PHP_VERSION.rb" "Formula/$PHP_VERSION.rb"
     fi
+  elif [[ "$PHP_VERSION" =~ php@8.[1-9] ]]; then
+    url="$(grep -e "^  url.*" ./Formula/"$PHP_VERSION".rb | cut -d\" -f 2)"
+    checksum=$(curl -sSL "$url" | shasum -a 256 | cut -d' ' -f 1)
+    commit="$(curl -sL https://api.github.com/repos/php/php-src/commits/master | sed -n 's|^  "sha":.*"\([a-f0-9]*\)",|\1|p')"
+    sed -i -e "s|^  sha256.*|  sha256 \"$checksum\"|g" ./Formula/"$PHP_VERSION".rb
+    sed -i -e "s|commit.*|commit=$commit\"|g" ./Formula/"$PHP_VERSION".rb
   fi
   unbottle
 }
@@ -51,5 +49,5 @@ fetch() {
 fetch
 if [[ "$GITHUB_MESSAGE" != *--build-"$PHP_VERSION" ]] &&
    [[ "$GITHUB_MESSAGE" != *--build-all* ]]; then
-  check_url
+  check_changes
 fi
