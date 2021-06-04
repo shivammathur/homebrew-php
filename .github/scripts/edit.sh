ab2a5cb1da651064cccc3843a2b241c838595302
Shivam Mathur <shivam_jpr@hotmail.com>
2021-06-04 06:16:57 +0530

    Fetch default php from php.net correctly

--- .github/scripts/edit.sh
+++ .github/scripts/edit.sh
@@ -25,8 +25,9 @@ fetch() {
     checksum=$(curl -sSL "$url" | shasum -a 256 | cut -d' ' -f 1)
     sed -i -e "s|^  sha256.*|  sha256 \"$checksum\"|g" ./Formula/"$PHP_VERSION".rb
   elif [[ "$PHP_VERSION" =~ php$|php@7.[3-4] ]]; then
-    OLD_PHP_SEMVER=$(grep -Po -m 1 "php-(${PHP_VERSION/php@/}.[0-9]+)" ./Formula/"$PHP_VERSION".rb)
-    NEW_PHP_SEMVER=$(curl -sL https://www.php.net/releases/feed.php | grep -Po -m 1 "php-(${PHP_VERSION/php@/}.[0-9]+)" | head -n 1)
+    PHP_MM=$(grep -Po -m 1 "php-[0-9]+.[0-9]+)" ./Formula/"$PHP_VERSION".rb | cut -d '-' -f 2)
+    OLD_PHP_SEMVER=$(grep -Po -m 1 "php-$PHP_MM.[0-9]+" ./Formula/"$PHP_VERSION".rb)
+    NEW_PHP_SEMVER=$(curl -sL https://www.php.net/releases/feed.php | grep -Po -m 1 "php-$PHP_MM.[0-9]+" | head -n 1)
     if [ "$NEW_PHP_SEMVER" != "$OLD_PHP_SEMVER" ]; then
       sed -i -e "s|$OLD_PHP_SEMVER|$NEW_PHP_SEMVER|g" ./Formula/"$PHP_VERSION".rb
       url="$(grep -e "^  url.*" ./Formula/"$PHP_VERSION".rb | cut -d\" -f 2)"
