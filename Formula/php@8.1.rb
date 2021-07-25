17e47257fc4cd4ed46b79b63d89a786e290b5bff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-25 17:36:09 +0000

    php@8.1: update 8.1.0_1 bottle.

--- Formula/php@8.1.rb
+++ Formula/php@8.1.rb
@@ -9,10 +9,11 @@ class PhpAT81 < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
-    rebuild 10
-    sha256 arm64_big_sur: "a932722c4e5d6f6a624fb9870c627cec632f72bca21e05ad58044465cd89a5ce"
-    sha256 big_sur:       "74d5351b6f5b780e6265094d74f0a6ce9faf7863f900f001ee53568b5c60dc22"
-    sha256 catalina:      "590456405da37c61d7d03067738ff967626acd7aba1a0bf90b5dbc378b9d88b5"
+    rebuild 11
+    sha256 arm64_big_sur: "6094bda76d308b0909c9c61e346b5e7f15b1765b2f9c56b9be5f5e8faeb3e42d"
+    sha256 big_sur:       "74759f36a5c4c5a48e9a9f8be651cd575579089608290e111236d4d18d0cb931"
+    sha256 catalina:      "09eedb1ac0ef43788b1ae92c2e5def3882741039b7aca2a52599c5ba50af58a5"
+    sha256 x86_64_linux:  "9fb541fc354864b5bb0645d4ccc94129aa8b438aa47cc9691ab5cd84f9d152b6"
   end
 
   keg_only :versioned_formula
