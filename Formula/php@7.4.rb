1114561de8ce8071eb4842ce51f793c2fbf776a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-09-21 21:55:12 +0000

    Update php@7.4

--- Formula/php@7.4.rb
+++ Formula/php@7.4.rb
@@ -2,9 +2,9 @@ class PhpAT74 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
   # Should only be updated if the new version is announced on the homepage, https://www.php.net/
-  url "https://www.php.net/distributions/php-7.4.23.tar.xz"
-  mirror "https://fossies.org/linux/www/php-7.4.23.tar.xz"
-  sha256 "cea52313fcffe56343bcd3c66dbb23cd5507dc559cc2e3547cf8f5452e88a05d"
+  url "https://www.php.net/distributions/php-7.4.24.tar.xz"
+  mirror "https://fossies.org/linux/www/php-7.4.24.tar.xz"
+  sha256 "ff7658ee2f6d8af05b48c21146af5f502e121def4e76e862df5ec9fa06e98734"
   license "PHP-3.01"
 
   bottle do
