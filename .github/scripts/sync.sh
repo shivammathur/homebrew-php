b489afcfaebf977904b383da90fe63f7ab17b498
Shivam Mathur <shivam_jpr@hotmail.com>
2021-10-13 14:42:03 +0530

    Fix sync script for keg-only formulae [skip ci]
    Optimise sync script

--- .github/scripts/sync.sh
+++ .github/scripts/sync.sh
@@ -2,6 +2,8 @@ export HOMEBREW_CHANGE_ARCH_TO_ARM=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+brew_cellar=$(brew --cellar)
+brew_repo="$(brew --repository)"
 core_repo="$(brew --repository homebrew/core)"
 deps_file="$GITHUB_WORKSPACE/.github/deps/${ImageOS:?}_${ImageVersion:?}"
 
@@ -11,47 +13,45 @@ git config --local user.name BrewTestBot
 git config --local pull.rebase true
 
 # Install PHP if not found, this will install all the dependencies
-brew install php
+brew install php 2>/dev/null || true
 
 # Update dependency formulae
 for formula in apr apr-util argon2 aspell autoconf curl freetds gd gettext glib gmp icu4c krb5 libffi libpq libsodium libzip oniguruma openldap openssl@1.1 pcre2 sqlite tidy-html5 unixodbc; do
   mkdir -p /tmp/libs/"$formula" /tmp/formulae
   sudo cp "$core_repo/Formula/$formula.rb" /tmp/formulae/
-  formula_prefix="$(brew --prefix "$formula")"
-  if ! [ -d "$formula_prefix"/lib ]; then
+  formula_cellar="$(brew info "$formula" | grep "$brew_cellar" | cut -d ' ' -f 1 | tail -n 1)"
+  if ! [ -d "$formula_cellar"/lib ]; then
     continue
   fi
   curl -o "$core_repo/Formula/$formula.rb" -sL https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/"$formula".rb
-  find "$formula_prefix"/lib -maxdepth 1 -name \*.dylib -print0 | xargs -I{} -0 cp -a {} /tmp/libs/"$formula"/
+  find "$formula_cellar"/lib -maxdepth 1 -name \*.dylib -print0 | xargs -I{} -0 cp -a {} /tmp/libs/"$formula"/
 done
 
-# Get updated formulae
-(
-  cd "$core_repo" || exit
-  git diff --name-only | cut -d '/' -f 2 | sed -e 's/\.[^.]*$//' | sudo tee /tmp/deps_updated
-)
+# Get updated formulae and reinstall them, and update brew.
+IFS=" " read -r -a formulae <<< "$(git -C "$core_repo" diff --name-only | cut -d '/' -f 2 | sed -e 's/\.[^.]*$//' | tr '\n' ' ')"
+git -C "$core_repo" add . && git -C "$core_repo" stash && git -C "$core_repo" pull origin master
+git -C "$brew_repo" pull origin master
+brew reinstall "${formulae[@]}" || true
 
 # Check update formulae for library changes
 rm -f "$deps_file"
 touch "$deps_file"
-while read -r formula; do
-  formula_prefix="$(brew --prefix "$formula")"
-  if ! [ -d "$formula_prefix"/lib ]; then
+for formula in "${formulae[@]}"; do
+  formula_cellar="$(brew info "$formula" | grep "$brew_cellar" | cut -d ' ' -f 1 | tail -n 1)"
+  if ! [ -d "$formula_cellar"/lib ]; then
     continue
   fi
   printf "\n--- %s ---\n" "$formula"
-  brew reinstall "$formula" 2>/dev/null 1>&2 || true
-  formula_prefix="$(brew --prefix "$formula")"
   old_build_info=$(grep -Eo "(^  revision|^    rebuild) [0-9]+" -Eo /tmp/formulae/"$formula".rb | tr -d ' \n')
   new_build_info=$(grep -Eo "(^  revision|^    rebuild) [0-9]+" -Eo "$core_repo/Formula/$formula.rb" | tr -d ' \n')
   old_hash=$(echo "$old_build_info $(find /tmp/libs/"$formula"/ -maxdepth 1 -name '*.dylib' -exec basename {} \;)" | openssl sha256)
-  new_hash=$(echo "$new_build_info $(find "$formula_prefix"/lib -maxdepth 1 -name '*.dylib' -exec basename {} \;)" | openssl sha256)
+  new_hash=$(echo "$new_build_info $(find "$formula_cellar"/lib -maxdepth 1 -name '*.dylib' -exec basename {} \;)" | openssl sha256)
   echo "old hash: $old_hash"
   echo "new hash: $new_hash"
   if [ "$old_hash" != "$new_hash" ]; then
     echo "$formula" | sudo tee -a "$deps_file"
   fi
-done </tmp/deps_updated
+done
 
 # Push changes
 ls ./.github/deps/*
