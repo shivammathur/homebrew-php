5f9d73837fac8f4fa3fe144d9d936c9c0070ba9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-12-08 22:01:38 +0000

    php@7.4: update 7.4.33 bottle.

--- Formula/php@7.4.rb
+++ Formula/php@7.4.rb
@@ -8,6 +8,12 @@ class PhpAT74 < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
+    rebuild 1
+    sha256 arm64_monterey: "7635f87f066bc7b145d1bd657353ae6efaa154303e9c362a45edb3f632e5554d"
+    sha256 arm64_big_sur:  "face5143c23b06d71f0bf56215e6f9744e6f2a1d00bb3bfc14bfa9c0057aaf96"
+    sha256 monterey:       "857cf5e0ee2f894923a0b0bededa6170dfb8502d9a71fa4720428be823288ed8"
+    sha256 big_sur:        "cacbff209edd5d71046963383825ea147c186f587cc268953f2c26b98968b1de"
+    sha256 x86_64_linux:   "98d0bbe3e526bf2e8e7662ef277a86d7d651b191ce90cd40bf46735df1b727d4"
   end
 
   keg_only :versioned_formula
