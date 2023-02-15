769476eb237cb9b8a0535c0db94793f449693047
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-15 06:01:36 +0000

    php@7.1: update 7.1.33_6 bottle.

--- Formula/php@7.1.rb
+++ Formula/php@7.1.rb
@@ -9,12 +9,12 @@ class PhpAT71 < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
-    rebuild 1
-    sha256 arm64_monterey: "e870af387c308860d0fd417d4ac1459dc6eca280519ffb4b808e618306bdd613"
-    sha256 arm64_big_sur:  "f098dcde3b1af07e7bdf7884821498fa459b1dd301916a587d99c61f25ab26f5"
-    sha256 monterey:       "6fdee9427eb1d918c2912a3975494927456208c6c705d81ad014f186a465ff17"
-    sha256 big_sur:        "bd9c5709c67b6f7e46f6726cd383b1449eae45f76915d6fa83ad7ae5d5de260f"
-    sha256 x86_64_linux:   "7e2f84ebb0f367d883dfa37a8403e9389955467a5eb6f7dc5436216f9b55d16d"
+    rebuild 2
+    sha256 arm64_monterey: "1e6935e83b5b7c9705c7815941516a1001a7fdd6d359f6d85a8db66c63de229d"
+    sha256 arm64_big_sur:  "5111c89d9f0acd511199a0af266b1072815e5b3f461c6a8aa290ed40f92b114f"
+    sha256 monterey:       "05f147c5f813f5f5d9a4649a9d9b4489e54f9a0e682a006a8f1184131f3e0f1e"
+    sha256 big_sur:        "cb5bf4c299a6a98cd41bc9ecc7af308b2058f97ac106268a125ee9df7d1c9479"
+    sha256 x86_64_linux:   "72e8abefaf49589c435b0df04e56e2aefce36efaa149e96bbc4d5360230a48f8"
   end
 
   keg_only :versioned_formula
