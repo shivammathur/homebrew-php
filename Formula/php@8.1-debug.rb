3b3b34e6885cdc596815b08d610228d711dae3f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-02-15 00:06:14 +0000

    Update php@8.1-debug

--- Formula/php@8.1-debug.rb
+++ Formula/php@8.1-debug.rb
@@ -1,9 +1,9 @@
 class PhpAT81Debug < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://www.php.net/distributions/php-8.1.15.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.1.15.tar.xz"
-  sha256 "cd450fb4ee50488c5bf5f08851f514e5a1cac18c9512234d9e16c3a1d35781a6"
+  url "https://www.php.net/distributions/php-8.1.16.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.1.16.tar.xz"
+  sha256 "7108b7347981ad6e610aaf3b3fb0f6444019ab6f59a872c1b55a29bc753eba93"
   license "PHP-3.01"
 
   bottle do
