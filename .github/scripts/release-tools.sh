39e8cfe332e90a5f4cd060be3616604c5588b564
Shivam Mathur <shivam_jpr@hotmail.com>
2023-03-15 01:59:03 +0000

    Refactor tools setup in publish workflow
    Use the PPA for gh

---
+++ .github/scripts/release-tools.sh
@@ -0,0 +1,12 @@
+# Add gh PPA key
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+
+# Add gh PPA
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+# Update lists
+sudo apt-get update -y
+
+# Install gh, skopeo, and unzip
+sudo apt-get install -y gh skopeo unzip
