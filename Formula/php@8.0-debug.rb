c63f5c593589dda9d954887fcb0c091aad676e93
Shivam Mathur <shivam_jpr@hotmail.com>
2023-04-26 23:34:34 +0530

    php@8.0-debug: Rebuild for gd upgrade

--- Formula/php@8.0-debug.rb
+++ Formula/php@8.0-debug.rb
@@ -6,6 +6,7 @@ class PhpAT80Debug < Formula
   mirror "https://fossies.org/linux/www/php-8.0.28.tar.xz"
   sha256 "5e07278a1f315a67d36a676c01343ca2d4da5ec5bdb15d018e4248b3012bc0cd"
   license "PHP-3.01"
+  revision 1
 
   livecheck do
     url "https://www.php.net/downloads"
