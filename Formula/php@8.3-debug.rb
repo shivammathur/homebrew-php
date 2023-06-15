9c846c6918a6ad3e56369142cff52163979d6e87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-06-15 00:09:59 +0000

    Update php@8.3-debug

--- Formula/php@8.3-debug.rb
+++ Formula/php@8.3-debug.rb
@@ -1,9 +1,9 @@
 class PhpAT83Debug < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://github.com/php/php-src/archive/49fbbea2ea5fe1f6bab0719c95a46a119ea3d91f.tar.gz?commit=49fbbea2ea5fe1f6bab0719c95a46a119ea3d91f"
+  url "https://github.com/php/php-src/archive/96ea06a1d9b115a138bd9e16a3ecc7901ae3abf6.tar.gz?commit=96ea06a1d9b115a138bd9e16a3ecc7901ae3abf6"
   version "8.3.0"
-  sha256 "78939f82a3ad835fad99f8fbb4a6200075086339f98593e2595c100f549f39c6"
+  sha256 "dbd7f3ad581b6c760bd5215137deb582cc80608bc8968303a64c16b5aefc84a8"
   license "PHP-3.01"
 
   bottle do
