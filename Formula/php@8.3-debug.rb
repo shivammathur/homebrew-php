ea495ca3119a5ed1aaa520f97a9df7d38fe13ff1
Shivam Mathur <shivam_jpr@hotmail.com>
2023-07-13 09:19:14 +0000

    Switch to openssl@3

--- Formula/php@8.3-debug.rb
+++ Formula/php@8.3-debug.rb
@@ -40,7 +40,7 @@ class PhpAT83Debug < Formula
   depends_on "libzip"
   depends_on "oniguruma"
   depends_on "openldap"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "pcre2"
   depends_on "sqlite"
   depends_on "tidy-html5"
@@ -207,7 +207,7 @@ class PhpAT83Debug < Formula
     end
 
     # Use OpenSSL cert bundle
-    openssl = Formula["openssl@1.1"]
+    openssl = Formula["openssl@3"]
     %w[development production].each do |mode|
       inreplace "php.ini-#{mode}", /; ?openssl\.cafile=/,
         "openssl.cafile = \"#{openssl.pkgetc}/cert.pem\""
