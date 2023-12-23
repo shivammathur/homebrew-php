ce0547753df7eb0b645c8a8efdcd712af56f4df8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2023-12-23 00:09:25 +0000

    Update php

--- Formula/php.rb
+++ Formula/php.rb
@@ -2,9 +2,9 @@ class Php < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
   # Should only be updated if the new version is announced on the homepage, https://www.php.net/
-  url "https://www.php.net/distributions/php-8.3.0.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.3.0.tar.xz"
-  sha256 "1db84fec57125aa93638b51bb2b15103e12ac196e2f960f0d124275b2687ea54"
+  url "https://www.php.net/distributions/php-8.3.1.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.3.1.tar.xz"
+  sha256 "56445b1771b2ba5b7573453f9e8a9451e2d810b1741a352fa05259733b1e9758"
   license "PHP-3.01"
 
   livecheck do
