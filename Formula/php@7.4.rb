881354c9a0200491f5e9d9360565cc0c865f144d
Shivam Mathur <contact@shivammathur.com>
2024-02-26 13:58:15 +0530

    php@7.4: revision bump

--- Formula/php@7.4.rb
+++ Formula/php@7.4.rb
@@ -5,7 +5,7 @@ class PhpAT74 < Formula
   version "7.4.33"
   sha256 "7c9de3f869a5ccae677b6a83582d5e5d0c825af22096c4e924c10abe118315f4"
   license "PHP-3.01"
-  revision 5
+  revision 6
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
