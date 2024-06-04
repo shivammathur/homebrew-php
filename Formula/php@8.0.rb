f36774cdd2fc6ac0a86ebf0e806fe0ae063bdbd2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-04 10:13:28 +0000

    Update php@8.0

--- Formula/php@8.0.rb
+++ Formula/php@8.0.rb
@@ -1,9 +1,9 @@
 class PhpAT80 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://github.com/shivammathur/php-src-backports/archive/3df78fd9f89e8356b78353e133fc05a6101b7237.tar.gz"
+  url "https://github.com/shivammathur/php-src-backports/archive/6bcf470af46ebdfe52ab9ec06920715a02045fa6.tar.gz"
   version "8.0.30"
-  sha256 "4a6a9f30a9652f6d44df10c994fc640e48a9744ab91a0ceb990a19d2cd88e1a3"
+  sha256 "651d5cbc9dfe5837223379c9c7a67bfb6f7c0254b1624f78e3e274fce93d68c6"
   license "PHP-3.01"
   revision 3
 
