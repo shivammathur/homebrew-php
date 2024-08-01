e6a33f3bef5d402a11e95f4e245c70675e1ca6c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-01 12:08:48 +0000

    php@7.3-zts: update 7.3.33_1 bottle.

--- Formula/php@7.3-zts.rb
+++ Formula/php@7.3-zts.rb
@@ -9,13 +9,13 @@ class PhpAT73Zts < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
-    rebuild 4
-    sha256 arm64_sonoma:   "d4e59b8327ca964fd81b08ea9e6b7755f65231f01ec7fd8cf5aaf92806b14940"
-    sha256 arm64_ventura:  "2cf677b6aab5a26248c252e04e24c21d15d4ab14162c308af7ead50f82a5e4ac"
-    sha256 arm64_monterey: "059ef9238c436fe6ae197b4b05d074b997707b6c5581b9afac672cac2db549bb"
-    sha256 ventura:        "c3aae712176c41a9329fe582440f93843f9cca945d76769daa0b4a1cfd40376d"
-    sha256 monterey:       "77063b5da5f72c84523c502f12406d5b8a57de903963a4f93b3a95445cd4b42b"
-    sha256 x86_64_linux:   "07135224a1276a7c11bd95f22bf6970fed9bab08b71c22f37f1c399a2517697e"
+    rebuild 5
+    sha256 arm64_sonoma:   "f2ff3b69416df6f4fd92ab4681c0c158da3e72fb591500f629d9a3eb6bd3d25d"
+    sha256 arm64_ventura:  "9eeabc815e67347483c62c25861f3c483c9a9e541db33bc603aecbb519e501ff"
+    sha256 arm64_monterey: "1f8df64707f2b12cbaa89a2fc6195697aa94c358992f193bafa8f1e748c79d84"
+    sha256 ventura:        "fc947c43e5d78a131d2c64435f14b47f649ea4437f1f147bda88eda621303da6"
+    sha256 monterey:       "6eb8d0d6bbff32e350c758cca400fcddc2d27ce5b0b653cfc773bab0e5acb75e"
+    sha256 x86_64_linux:   "a0dac76c2941c83df4ad58da37cccb2ea58bc410c18c5d978d51f8a8ff90719f"
   end
 
   keg_only :versioned_formula
