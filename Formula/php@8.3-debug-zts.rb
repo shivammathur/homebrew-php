0e03fbd98eadbff65c91039e175ba452c261b249
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-17 00:29:08 +0000

    Update php@8.3-debug-zts

--- Formula/php@8.3-debug-zts.rb
+++ Formula/php@8.3-debug-zts.rb
@@ -1,9 +1,9 @@
 class PhpAT83DebugZts < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://www.php.net/distributions/php-8.3.15.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.3.15.tar.xz"
-  sha256 "3df5d45637283f759eef8fc3ce03de829ded3e200c3da278936a684955d2f94f"
+  url "https://www.php.net/distributions/php-8.3.16.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.3.16.tar.xz"
+  sha256 "40d3b4e6cac33d3bcefe096d75a28d4fb4e3a9615eb20a4de55ba139fbfacdd5"
   license "PHP-3.01"
 
   bottle do
