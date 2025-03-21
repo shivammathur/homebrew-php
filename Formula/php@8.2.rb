3e6ee6381dc1e69736e63193ad95cfd0959fe59f
Shivam Mathur <shivam_jpr@hotmail.com>
2025-03-21 09:06:26 +0530

    php@8.2: revision bump to migrate to icu4c@77

--- Formula/php@8.2.rb
+++ Formula/php@8.2.rb
@@ -35,7 +35,7 @@ class PhpAT82 < Formula
   depends_on "gd"
   depends_on "gettext"
   depends_on "gmp"
-  depends_on "icu4c@76"
+  depends_on "icu4c@77"
   depends_on "krb5"
   depends_on "libpq"
   depends_on "libsodium"
