69986f393e449c711e071227403026d8465d5c8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-21 00:35:04 +0000

    Update php@8.5-zts

--- Formula/php@8.5-zts.rb
+++ Formula/php@8.5-zts.rb
@@ -1,9 +1,9 @@
 class PhpAT85Zts < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://github.com/php/php-src/archive/f1295864597a2e46ee2694c5bcdc0132427e9c3f.tar.gz?commit=f1295864597a2e46ee2694c5bcdc0132427e9c3f"
+  url "https://github.com/php/php-src/archive/89be689f778430f93bac04d75248b5b6fc593571.tar.gz?commit=89be689f778430f93bac04d75248b5b6fc593571"
   version "8.5.0"
-  sha256 "57d7f3b0ebc5e0b0c354b7f0db8fced9658274bdb89ae25f0f808b9bd2c30cf3"
+  sha256 "3e9cba04d39f06f8ac92fa8780bf98f0c60dee23f5f574dd5f3202c5589ad5d1"
   license "PHP-3.01"
   revision 4
 
