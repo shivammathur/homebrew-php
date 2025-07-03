8333fc1f80de4bf5be57769357f25be829903124
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-03 17:24:00 +0000

    Update php@8.3

--- Formula/php@8.3.rb
+++ Formula/php@8.3.rb
@@ -1,9 +1,9 @@
 class PhpAT83 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://www.php.net/distributions/php-8.3.22.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.3.22.tar.xz"
-  sha256 "66c86889059bd27ccf460590ca48fcaf3261349cc9bdba2023ac6a265beabf36"
+  url "https://www.php.net/distributions/php-8.3.23.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.3.23.tar.xz"
+  sha256 "08be64700f703bca6ff1284bf1fdaffa37ae1b9734b6559f8350248e8960a6db"
   license "PHP-3.01"
 
   bottle do
