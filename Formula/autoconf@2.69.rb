6518c726c6661aa47183e751745b324b8d5f5208
Shivam Mathur <shivam_jpr@hotmail.com>
2025-08-15 13:23:45 +0530

    use ftpmirror.gnu.org as primary URL[skip ci]

--- Formula/autoconf@2.69.rb
+++ Formula/autoconf@2.69.rb
@@ -1,8 +1,8 @@
 class AutoconfAT269 < Formula
   desc "Automatic configure script builder"
   homepage "https://www.gnu.org/software/autoconf/"
-  url "https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
-  mirror "https://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz"
+  url "https://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
   sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
   license all_of: [
     "GPL-3.0-or-later",
--- Formula/bison@2.7.rb
+++ Formula/bison@2.7.rb
@@ -1,8 +1,8 @@
 class BisonAT27 < Formula
   desc "Parser generator"
   homepage "https://www.gnu.org/software/bison/"
-  url "https://ftp.gnu.org/gnu/bison/bison-2.7.1.tar.gz"
-  mirror "https://ftpmirror.gnu.org/bison/bison-2.7.1.tar.gz"
+  url "https://ftpmirror.gnu.org/bison/bison-2.7.1.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/bison/bison-2.7.1.tar.gz"
   sha256 "08e2296b024bab8ea36f3bb3b91d071165b22afda39a17ffc8ff53ade2883431"
   license "GPL-3.0-or-later"
   revision 1
