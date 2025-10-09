46d9fd0b8c3be571e3479f78c2e3689d3cc1ab72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-09 19:06:20 +0000

    Update php@8.0-debug-zts

--- Formula/php@8.0-debug-zts.rb
+++ Formula/php@8.0-debug-zts.rb
@@ -1,9 +1,9 @@
 class PhpAT80DebugZts < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://github.com/shivammathur/php-src-backports/archive/4f0b7adf91b6398d1591f613893f90c33da9e0e9.tar.gz"
+  url "https://github.com/shivammathur/php-src-backports/archive/f5f8a1ecdfd2181f10f304290940fc2af6a8c2bf.tar.gz"
   version "8.0.30"
-  sha256 "2f9e07b59658cd52fa6631c3a42c2a5dfd5802ce3255f5aed2d4f1a3d060bfcb"
+  sha256 "c0cb5602be5f8e4b1277a516630c8be812272123051d6c0c6e74724d6a482760"
   license "PHP-3.01"
   revision 5
 
