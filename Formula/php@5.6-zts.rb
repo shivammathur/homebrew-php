65633b7ce641080f2ec1d9a4acaeee6b905ca4f6
Shivam Mathur <shivam_jpr@hotmail.com>
2025-11-07 02:29:53 +0530

    php@5.6-zts: Rebuild for bison@2.7 fix

--- Formula/php@5.6-zts.rb
+++ Formula/php@5.6-zts.rb
@@ -1,11 +1,11 @@
 class PhpAT56Zts < Formula
   desc "General-purpose scripting language"
   homepage "https://secure.php.net/"
-  url "https://github.com/shivammathur/php-src-backports/archive/2caa81b25793a7c1878530ed80a289b070cfa44f.tar.gz"
+  url "https://github.com/shivammathur/php-src-backports/archive/6cfe49e294414185452ec89bad39b1bd42cc72c9.tar.gz"
   version "5.6.40"
-  sha256 "b3397170680a3fe9f1ba36298794af232f76c1eb6d647cd0fe5581a5f233ffc3"
+  sha256 "c7aea2d4742a6daadfa333dce1e6707bd648b2ed54e36238674db026e27d43cf"
   license "PHP-3.01"
-  revision 4
+  revision 5
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
