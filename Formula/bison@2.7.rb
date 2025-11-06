a516c6e2cc1c96cc3dc4accb7322b536b4e1c6b9
Shivam Mathur <shivam_jpr@hotmail.com>
2025-11-06 14:41:15 +0530

    bison@2.7: remove :high_sierra usage [skip ci]

--- Formula/bison@2.7.rb
+++ Formula/bison@2.7.rb
@@ -26,7 +26,7 @@ class BisonAT27 < Formula
   patch :DATA
 
   patch :p0 do
-    on_high_sierra :or_newer do
+    on_macos do
       url "https://raw.githubusercontent.com/macports/macports-ports/b76d1e48dac/editors/nano/files/secure_snprintf.patch"
       sha256 "57f972940a10d448efbd3d5ba46e65979ae4eea93681a85e1d998060b356e0d2"
     end
