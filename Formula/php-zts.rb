a27abd29901c8f5c9861afda58217cc67f11a9a6
Shivam Mathur <shivam_jpr@hotmail.com>
2025-11-19 19:13:36 +0530

    php-zts: revision bump (icu4c@78)

--- Formula/php-zts.rb
+++ Formula/php-zts.rb
@@ -6,7 +6,7 @@ class PhpZts < Formula
   mirror "https://fossies.org/linux/www/php-8.4.14.tar.xz"
   sha256 "bac90ee7cf738e814c89b6b27d4d2c4b70e50942a420837e1a22f5fd5f9867a3"
   license "PHP-3.01"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://www.php.net/downloads?source=Y"
@@ -42,7 +42,7 @@ class PhpZts < Formula
   depends_on "gd"
   depends_on "gettext"
   depends_on "gmp"
-  depends_on "icu4c@77"
+  depends_on "icu4c@78"
   depends_on "libpq"
   depends_on "libsodium"
   depends_on "libzip"
