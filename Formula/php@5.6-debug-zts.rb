30de2a83d22236f9a374503cdb74c83ce48151de
Shivam Mathur <shivam_jpr@hotmail.com>
2025-11-19 19:08:51 +0530

    php@5.6-debug-zts: revision bump (icu4c@78)

--- Formula/php@5.6-debug-zts.rb
+++ Formula/php@5.6-debug-zts.rb
@@ -5,7 +5,7 @@ class PhpAT56DebugZts < Formula
   version "5.6.40"
   sha256 "c7aea2d4742a6daadfa333dce1e6707bd648b2ed54e36238674db026e27d43cf"
   license "PHP-3.01"
-  revision 4
+  revision 5
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
@@ -38,7 +38,7 @@ class PhpAT56DebugZts < Formula
   depends_on "gd"
   depends_on "gettext"
   depends_on "gmp"
-  depends_on "icu4c@77"
+  depends_on "icu4c@78"
   depends_on "jpeg"
   depends_on "krb5"
   depends_on "libpng"
@@ -181,7 +181,7 @@ class PhpAT56DebugZts < Formula
       --with-gettext=#{Formula["gettext"].opt_prefix}
       --with-gmp=#{Formula["gmp"].opt_prefix}
       --with-iconv#{headers_path}
-      --with-icu-dir=#{Formula["icu4c@77"].opt_prefix}
+      --with-icu-dir=#{Formula["icu4c@78"].opt_prefix}
       --with-jpeg-dir=#{Formula["jpeg"].opt_prefix}
       --with-kerberos#{headers_path}
       --with-layout=GNU
