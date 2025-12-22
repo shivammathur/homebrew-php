ba17c30bf2f834fa511f3914628a22b5a9b26e67
Shivam Mathur <shivam_jpr@hotmail.com>
2025-12-22 11:53:53 +0530

    php-debug-zts: rebuild with ldap

--- Formula/php-debug-zts.rb
+++ Formula/php-debug-zts.rb
@@ -29,6 +29,7 @@ class PhpDebugZts < Formula
     "TCL",                   # 7
     "Zlib",                  # 8
   ]
+  revision 1
 
   livecheck do
     url "https://www.php.net/downloads?source=Y"
