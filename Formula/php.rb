e2da68e3558e5c61f0e3557609ce8885b9aa675f
Shivam Mathur <shivam_jpr@hotmail.com>
2025-12-24 11:11:31 +0530

    php: revision bump (net-snmp 5.9.5.2)

--- Formula/php.rb
+++ Formula/php.rb
@@ -29,7 +29,7 @@ class Php < Formula
     "TCL",                   # 7
     "Zlib",                  # 8
   ]
-  revision 1
+  revision 2
 
   livecheck do
     url "https://www.php.net/downloads?source=Y"
