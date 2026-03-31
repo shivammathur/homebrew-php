3702f16a2c04be37acedbbf2fc15ddae32f1fd60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-31 00:50:41 +0000

    Update php@8.6-debug-zts

--- Formula/php@8.6-debug-zts.rb
+++ Formula/php@8.6-debug-zts.rb
@@ -1,9 +1,9 @@
 class PhpAT86DebugZts < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://github.com/php/php-src/archive/c45b2bec75269870657c9c405a0d28a0b857ea82.tar.gz?commit=c45b2bec75269870657c9c405a0d28a0b857ea82"
+  url "https://github.com/php/php-src/archive/b1710f48baddb6d980b00157c24371ec92852c15.tar.gz?commit=b1710f48baddb6d980b00157c24371ec92852c15"
   version "8.6.0"
-  sha256 "22bc15398ab0f8786ecae2ffd47d5d0be23e3a7074beed6d3294a462d1e11cfb"
+  sha256 "4075374c24f1954c3f5f12dc81f2ddb0a363c004d575e172c871a691e63c2c59"
   license all_of: [
     "PHP-3.01",
 
