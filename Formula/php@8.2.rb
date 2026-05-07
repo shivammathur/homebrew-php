bf1497db0c27cc9f269c80535fb1f11dbac6f110
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-07 17:18:02 +0000

    php@8.2 8.2.31

--- Formula/php@8.2.rb
+++ Formula/php@8.2.rb
@@ -1,9 +1,9 @@
 class PhpAT82 < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://www.php.net/distributions/php-8.2.30.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.2.30.tar.xz"
-  sha256 "bc90523e17af4db46157e75d0c9ef0b9d0030b0514e62c26ba7b513b8c4eb015"
+  url "https://www.php.net/distributions/php-8.2.31.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.2.31.tar.xz"
+  sha256 "95eae411d594fe6f6e5678b76645dc13ae47d3c0a5325c1d969b58dea56ee45a"
   license all_of: [
     "PHP-3.01",
 
@@ -268,6 +268,8 @@ class PhpAT82 < Formula
         # rubocop:disable all
         ENV["CC"] = "/usr/bin/clang"
         ENV["CXX"] = "/usr/bin/clang++"
+        # Ensure there is enough Mach-O header space for Homebrew rpath rewrites.
+        ENV.append "LDFLAGS", "-Wl,-headerpad_max_install_names"
         # rubocop:enable all
       end
       system "./configure", "--with-php-config=#{bin}/php-config"
