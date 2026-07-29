13ba5d40a2c0251716f668eeede3470ba30fd224
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-29 10:41:44 +0530

    php@8.2-zts 8.2.33

--- Formula/php@8.2-zts.rb
+++ Formula/php@8.2-zts.rb
@@ -1,9 +1,9 @@
 class PhpAT82Zts < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
-  url "https://www.php.net/distributions/php-8.2.32.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.2.32.tar.xz"
-  sha256 "e02aa173c236c12791696254d607da680e6d5516f8f5c2339642de7c4f944bd2"
+  url "https://www.php.net/distributions/php-8.2.33.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.2.33.tar.xz"
+  sha256 "fbdeace9b38220436a4c8fd79b900df92878151db145e641750743a283b514c1"
   license all_of: [
     "PHP-3.01",
 
