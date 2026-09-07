3e59da5ce0e14c0d33d939e5f3ce346f16a1a000
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-07 20:35:23 +0530

    php@8.4-zts: rebuild post-install steps

--- Formula/php@8.4-zts.rb
+++ Formula/php@8.4-zts.rb
@@ -28,6 +28,7 @@ class PhpAT84Zts < Formula
     "TCL",                   # 7
     "Zlib",                  # 8
   ]
+  revision 1
 
   livecheck do
     url "https://www.php.net/downloads?source=Y"
@@ -87,7 +88,7 @@ class PhpAT84Zts < Formula
     depends_on "zlib-ng-compat"
   end
 
-  deny_network_access! [:build, :postinstall]
+  deny_network_access! [:build]
 
   def install
     system "./buildconf", "--force"
@@ -319,7 +320,9 @@ class PhpAT84Zts < Formula
                 args: %w[config-set test_dir {{HOMEBREW_PREFIX}}/share/pear@{{version.major_minor}}-zts/test system]
     run "pear", base: :bin, args: %w[config-set php_bin {{opt_prefix}}/bin/php system]
 
-    run "pear", base: :bin, args: ["update-channels"]
+    run "pear", base: :bin, args: ["update-channels"], print_stdout: true
+
+    mkdir_p "php/{{version.major_minor}}-zts/conf.d", base: :etc
 
     run "php", base: :bin, args: [
       "-n", "-r", <<~'PHP',
