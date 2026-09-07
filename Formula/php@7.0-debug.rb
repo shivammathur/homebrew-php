4946291ca82bee059d4cd16e791218b7b194805f
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-07 20:35:10 +0530

    php@7.0-debug: rebuild post-install steps

--- Formula/php@7.0-debug.rb
+++ Formula/php@7.0-debug.rb
@@ -5,7 +5,7 @@ class PhpAT70Debug < Formula
   version "7.0.33"
   sha256 "c412fdeac66cb816f3f3fa5a7a6755daf3f37521d997fca771ecd40f61b22cc3"
   license "PHP-3.01"
-  revision 16
+  revision 17
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
@@ -71,9 +71,10 @@ class PhpAT70Debug < Formula
     depends_on "zlib-ng-compat"
   end
 
-  deny_network_access! [:postinstall]
-
   def install
+    # The runtime probe can misdetect glibc's POSIX readdir_r in build containers.
+    ENV["ac_cv_what_readdir_r"] = "POSIX" if OS.linux?
+
     # Work around configure issues with Xcode 12
     # See https://bugs.php.net/bug.php?id=80171
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
@@ -319,7 +320,9 @@ class PhpAT70Debug < Formula
                 args: %w[config-set test_dir {{HOMEBREW_PREFIX}}/share/pear@{{version.major_minor}}-debug/test system]
     run "pear", base: :bin, args: %w[config-set php_bin {{opt_prefix}}/bin/php system]
 
-    run "pear", base: :bin, args: ["update-channels"]
+    run "pear", base: :bin, args: ["update-channels"], print_stdout: true
+
+    mkdir_p "php/{{version.major_minor}}-debug/conf.d", base: :etc
 
     run "php", base: :bin, args: [
       "-n", "-r", <<~'PHP',
