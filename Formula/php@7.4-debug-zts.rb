ba49cdeef1e42264e5c56864cde2e6420b7bd9db
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-07 21:09:04 +0530

    php@7.4-debug-zts: rebuild post-install steps

--- Formula/php@7.4-debug-zts.rb
+++ Formula/php@7.4-debug-zts.rb
@@ -5,7 +5,7 @@ class PhpAT74DebugZts < Formula
   version "7.4.33"
   sha256 "d82887f2166e8526ea9b1cfd8c5ecf5649718f0b6e341380d333eba8066429a4"
   license "PHP-3.01"
-  revision 6
+  revision 7
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
@@ -69,8 +69,6 @@ class PhpAT74DebugZts < Formula
   on_linux do
     depends_on "zlib-ng-compat"
   end
-
-  deny_network_access! [:postinstall]
 
   def install
     # PHP 7.4 still has K&R-style bcmath/intl sources that fail under C23.
@@ -213,7 +211,7 @@ class PhpAT74DebugZts < Formula
     if OS.mac?
       args << "--enable-dtrace"
       args << "--with-ldap-sasl"
-      args << "--with-os-sdkpath=#{MacOS.sdk_path_if_needed}"
+      args << "--with-os-sdkpath=#{sdk_path}"
     else
       args << "--disable-dtrace"
       args << "--without-ldap-sasl"
@@ -309,7 +307,9 @@ class PhpAT74DebugZts < Formula
                          {{HOMEBREW_PREFIX}}/share/pear@{{version.major_minor}}-debug-zts/test system]
     run "pear", base: :bin, args: %w[config-set php_bin {{opt_prefix}}/bin/php system]
 
-    run "pear", base: :bin, args: ["update-channels"]
+    run "pear", base: :bin, args: ["update-channels"], print_stdout: true
+
+    mkdir_p "php/{{version.major_minor}}-debug-zts/conf.d", base: :etc
 
     run "php", base: :bin, args: [
       "-n", "-r", <<~'PHP',
