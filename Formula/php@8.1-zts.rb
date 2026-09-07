c404f058eaeb847bde4fe6b35bf12f6537881ab0
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-07 21:09:04 +0530

    php@8.1-zts: rebuild post-install steps

--- Formula/php@8.1-zts.rb
+++ Formula/php@8.1-zts.rb
@@ -27,7 +27,7 @@ class PhpAT81Zts < Formula
     "TCL",                   # 7
     "Zlib",                  # 8
   ]
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
@@ -85,8 +85,6 @@ class PhpAT81Zts < Formula
   on_linux do
     depends_on "zlib-ng-compat"
   end
-
-  deny_network_access! [:postinstall]
 
   def install
     # PHP 8.1 still has K&R-style bcmath sources that fail under C23.
@@ -225,7 +223,7 @@ class PhpAT81Zts < Formula
     if OS.mac?
       args << "--enable-dtrace"
       args << "--with-ldap-sasl"
-      args << "--with-os-sdkpath=#{MacOS.sdk_path_if_needed}"
+      args << "--with-os-sdkpath=#{sdk_path}"
     else
       args << "--disable-dtrace"
       args << "--without-ldap-sasl"
@@ -316,7 +314,9 @@ class PhpAT81Zts < Formula
                 args: %w[config-set test_dir {{HOMEBREW_PREFIX}}/share/pear@{{version.major_minor}}-zts/test system]
     run "pear", base: :bin, args: %w[config-set php_bin {{opt_prefix}}/bin/php system]
 
-    run "pear", base: :bin, args: ["update-channels"]
+    run "pear", base: :bin, args: ["update-channels"], print_stdout: true
+
+    mkdir_p "php/{{version.major_minor}}-zts/conf.d", base: :etc
 
     run "php", base: :bin, args: [
       "-n", "-r", <<~'PHP',
