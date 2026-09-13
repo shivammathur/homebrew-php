62432dc20e95c02a08ba1feddff573da4b47d33e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 15:14:20 +0000

    php@8.4: update 8.4.25_1 bottle.

--- Formula/php@8.4.rb
+++ Formula/php@8.4.rb
@@ -38,11 +38,12 @@ class PhpAT84 < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/shivammathur/php"
-    sha256 arm64_tahoe:   "38fdc2d9caaf9065ef2916c2448e9830096c4a388378411b36109eb263c264e6"
-    sha256 arm64_sequoia: "2c10c7ea24b941a336e9364db4298595be36da75fa01496470e9ba2ded3542c4"
-    sha256 arm64_sonoma:  "49ca529edfe94dadf903f9d76cc0c183f9b64f1e6cd1402cf38671e94cd3ee09"
-    sha256 arm64_linux:   "ed3a6a9a63e6fe86b25d18bb4790b0b28ea027bd5386b842d350b2ea1936b94e"
-    sha256 x86_64_linux:  "8bdf7ffa87ec7104b73abb544d4ffc570c966ae6de1fa063aa32b59cdc2fbc86"
+    sha256 arm64_golden_gate: "6e36a528fbf07e28a7023ddfa305b1ff51f548e180db5514d6279925d2d552e8"
+    sha256 arm64_tahoe:       "38fdc2d9caaf9065ef2916c2448e9830096c4a388378411b36109eb263c264e6"
+    sha256 arm64_sequoia:     "2c10c7ea24b941a336e9364db4298595be36da75fa01496470e9ba2ded3542c4"
+    sha256 arm64_sonoma:      "49ca529edfe94dadf903f9d76cc0c183f9b64f1e6cd1402cf38671e94cd3ee09"
+    sha256 arm64_linux:       "ed3a6a9a63e6fe86b25d18bb4790b0b28ea027bd5386b842d350b2ea1936b94e"
+    sha256 x86_64_linux:      "8bdf7ffa87ec7104b73abb544d4ffc570c966ae6de1fa063aa32b59cdc2fbc86"
   end
 
   keg_only :versioned_formula
